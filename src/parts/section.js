3a0c43dc4f2a571ef8a9fd78e050689e3756b38d
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-07-21 20:01:20 +0500

    I maked a card for section part

--- src/parts/section.js
+++ src/parts/section.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Navigation, Pagination} from "swiper/modules";
-import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 import img from "../images/header.png";
+
 
 function Section() {
     return (
         <section>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-lg-3">
+                        <div className="my-card">
+                            <img src={img} alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="my-card">
+                            <img src={img} alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="my-card">
+                            <img src={img} alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="my-card">
+                            <img src={img} alt="" className="img-fluid"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
